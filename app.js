4cbdb87138ae84960fe105b10ddd6d8932a5878f
sreelakshmipm02 <sreelakshmipm02@gmail.com>
2026-02-02 19:37:48 +0530

    database connected,app.js and server.js are connected.

---
+++ app.js
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import path from "path";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import { fileURLToPath } from "url";
+import connectDB from "./config/db.js";
+
+//fix __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+//connect to mongodb
+connectDB(process.env.MONGO_URI);
+
+//view engine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//static files
+app.use(express.static(path.join(__dirname, "public")));
+
+//session setup
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || "bella_secret_key",
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI,
+            collectionName: "sessions"
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 // 1 day
+        }
+    }
+    ));
+
+    //routes
+    app.use("/",userRoutes);
+    //404 handler
+    app.use((req,res) => {
+        res.status(404).send("Page Not Found!");
+    });
+
+    export default app;
---
+++ server.js
@@ -0,0 +1,8 @@
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}
+        http://localhost:3000`);
+    
+});
